abecbcb9df8fedc117b29c81b3b8a7768e87fc54
Spencer Robinson <spencerrrobinson@unomaha.edu>
2017-03-29 23:34:01 -0500

    added test script for form functions

---
+++ test/test_scripts.js
@@ -0,0 +1,89 @@
+forksdescribe("commitsGreen", function() {
+  it("Makes commits class green", function() {
+    expect($scope.commitsStyle).toEqual("green");
+  });
+});
+
+describe("commitsRed", function() {
+  it("Makes commits class green", function() {
+    expect($scope.commitsStyle).toEqual("Red");
+  });
+});
+
+describe("commitsWhite", function() {
+it("Makes commits class green", function() {
+expect($scope.commitsStyle).toEqual("White");
+});
+});
+
+describe("starsGreen", function() {
+  it("Makes stars class green", function() {
+    expect($scope.starsStyle).toEqual("green");
+  });
+});
+
+describe("starsRed", function() {
+  it("Makes stars class green", function() {
+    expect($scope.starsStyle).toEqual("Red");
+  });
+});
+
+describe("starsWhite", function() {
+it("Makes stars class green", function() {
+expect($scope.starsStyle).toEqual("White");
+});
+});
+
+describe("forksGreen", function() {
+  it("Makes forks class green", function() {
+    expect($scope.forksStyle).toEqual("green");
+  });
+});
+
+describe("forksRed", function() {
+  it("Makes forks class green", function() {
+    expect($scope.forksStyle).toEqual("Red");
+  });
+});
+
+describe("forksWhite", function() {
+it("Makes forks class green", function() {
+expect($scope.forksStyle).toEqual("White");
+});
+});
+
+describe("issuesGreen", function() {
+  it("Makes issues class green", function() {
+    expect($scope.issuesStyle).toEqual("green");
+  });
+});
+
+describe("issuesRed", function() {
+  it("Makes issues class green", function() {
+    expect($scope.issuesStyle).toEqual("Red");
+  });
+});
+
+describe("issuesWhite", function() {
+it("Makes issues class green", function() {
+expect($scope.issuesStyle).toEqual("White");
+});
+});
+
+describe("pullsGreen", function() {
+  it("Makes pulls class green", function() {
+    expect($scope.pullsStyle).toEqual("green");
+  });
+});
+
+describe("pullsRed", function() {
+  it("Makes pulls class green", function() {
+    expect($scope.pullsStyle).toEqual("Red");
+  });
+});
+
+describe("pullsWhite", function() {
+it("Makes pulls class green", function() {
+expect($scope.pullsStyle).toEqual("White");
+});
+});
